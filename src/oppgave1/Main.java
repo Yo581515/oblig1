bb319dd81d8331d5f1960d121f060c49207f4b56
581515 <47955690+Y581515@users.noreply.github.com>
2021-08-31 05:56:02 +0200

    oppdatert fra 1 traad til 2 traad

--- src/oppgave1/Main.java
+++ src/oppgave1/Main.java
@@ -5,17 +5,29 @@ import javax.swing.JOptionPane.*;
 
 public class Main {
 
+	private static String tekst = "Hallo verden!";
+
 	public static void main(String[] args) {
 
-		String tekst = "Hallo verden!";
 		MinThread t = new MinThread(tekst);
 
-		t.start();
+//		while (!(tekst.equals("quit"))) {
+//			tekst = JOptionPane.showInputDialog("skriv her");
+//			t.setTekst(tekst);
+//		}
 
-		while (!(tekst.equals("quit"))) {
-			tekst = JOptionPane.showInputDialog("skriv her");
-			t.setTekst(tekst);
-		}
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (!(tekst.equals("quit"))) {
+					tekst = JOptionPane.showInputDialog("skriv her");
+					t.setTekst(tekst);
+				}
+			}
+		});
+		
+		t.start();
+		t2.start();
 
 	}
 
